3c3b670dc8aaabde9b4a41ea94a5c8e40756f51f
DatPHP <nguyenvandat170296@gmail.com>
2024-03-24 11:25:49 +0700

    Build UI Of HomePage Done

--- backend/frontend-app/src/App.jsx
+++ backend/frontend-app/src/App.jsx
@@ -1,22 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
 import Banner from './components/banner.jsx'
 import Cart from './components/cart.jsx'
+import Slider from './components/slider.jsx'
+import Product from './components/product.jsx'
+import Backtotop from './components/backtotop.jsx'
+import Modal1 from './components/modal1.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header></Header>
       <Cart></Cart>
+      <Slider></Slider>
       <Banner></Banner>
-    
+      <Product></Product>
       <Footer></Footer>
+      <Backtotop></Backtotop>
+      <Modal1></Modal1>
     </>
   )
 }
---
+++ backend/frontend-app/src/components/backtotop.jsx
@@ -0,0 +1,19 @@
+
+import React, { useEffect, useState } from 'react';
+
+
+function Backtotop() {
+    return (
+        <>
+            <div className="btn-back-to-top" id="myBtn">
+            <span className="symbol-btn-back-to-top">
+                <i className="zmdi zmdi-chevron-up"></i>
+            </span>
+            </div>
+        </>
+    )
+}
+
+export default Backtotop
+
+
