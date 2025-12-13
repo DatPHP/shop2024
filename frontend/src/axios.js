4063977cdb5772b60c33053d6d53636f40fe2470
DatPHP <nguyenvandat170296@gmail.com>
2025-12-13 20:47:57 +0800

    ok 2

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-	baseURL: "http://localhost:8000/api",
+	// baseURL: "http://localhost:8000/api",
+	baseURL: "https://shop2024.onrender.com/api",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
